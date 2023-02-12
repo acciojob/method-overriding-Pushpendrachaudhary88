ef17436151170995a321ad147677e509a7970ddc
Pushpendrachaudhary88 <120740536+Pushpendrachaudhary88@users.noreply.github.com>
2023-02-12 18:39:50 +0530

    Method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,7 +10,6 @@ public class Main {
             public String meth() { return
                 "method is overridden in Extendend class B";}
             }
-
             public static void main(String[] args){
 
         B b = new B();
