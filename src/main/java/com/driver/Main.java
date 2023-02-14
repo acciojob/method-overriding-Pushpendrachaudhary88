c0d3ea018f8ed953e8be811005cff9dbfecef2e0
Pushpendrachaudhary88 <120740536+Pushpendrachaudhary88@users.noreply.github.com>
2023-02-14 16:48:23 +0530

    Mathod Overloading assgnment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,54 @@
+//package com.driver;
+//
+//public class Main {
+//    public static class A{
+//        public String meth(){
+//
+//            return "Invoking method from class A";
+//            }
+//        }
+//        public static class B extends A{
+//            @Override
+//            public String meth() { return
+//                "method is overridden in Extendend class B";}
+//            }
+//            public static void main(String[] args){
+//
+//        B b = new B();
+//        b.meth();
+//
+//        B b1 = new B();
+//        b1.meth();
+//        }
+//
+//}
 package com.driver;
 
 public class Main {
+
     public static class A{
         public String meth(){
-            return "Invoking method from class A";}
+            return "Invoking method from class A";
         }
-        public static class B extends A{
-            @Override
-            public String meth() { return
-                "method is overridden in Extendend class B";}
-            }
-            public static void main(String[] args){
+    }
+
+    public static class B extends A{
+
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
 
         B b = new B();
         b.meth();
 
         B b1 = new B();
         b1.meth();
-        }
+
+
+    }
 
 }
